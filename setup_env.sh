6e63c6784c5b946c5d3dd3f14fdeb667db40d445
philippart-s <wildagsx@gmail.com>
2024-01-04 16:30:34 +0100

    feat: ✨ use AI Token instead of openstack user

--- setup_env.sh
+++ setup_env.sh
@@ -7,17 +7,22 @@ STUDENT_ID=$(jq -r '.studentId' "attendee-conf.json")
 REGISTRY_NAME=ubf2r2if.c1.gra9.container-registry.ovh.net/lab
 REGISTRY_LOGIN=$(jq -r '.registryLogin' "attendee-conf.json")
 REGISTRY_PASSWORD=$(jq -r '.registryPassword' "attendee-conf.json")
-AI_USER=$(jq -r '.username' "attendee-conf.json")
-AI_PASSWORD=$(jq -r '.password' "attendee-conf.json")
+AI_TOKEN=$(jq -r '.token' "attendee-conf.json")
+#AI_USER=$(jq -r '.username' "attendee-conf.json")
+#AI_PASSWORD=$(jq -r '.password' "attendee-conf.json")
 
 echo "Your environment variables:"
 echo "   - STUDENT_ID: " $STUDENT_ID
 echo "   - REGISTRY_LOGIN: " $REGISTRY_LOGIN
 echo "   - REGISTRY_LOGIN: " $REGISTRY_PASSWORD
-echo "   - AI_USER: " $AI_USER
-echo "   - AI_PASSWORD: " $AI_PASSWORD
+echo "   - AI_TOKEN: " $AI_TOKEN
+#echo "   - AI_USER: " $AI_USER
+#echo "   - AI_PASSWORD: " $AI_PASSWORD
 
 # Authentication with AI CLI
-ovhai login -u $AI_USER -p $AI_PASSWORD
-echo "You are connected with your AI user:"
-ovhai me
+#ovhai login -u $AI_USER -p $AI_PASSWORD
+#echo "You are connected with your AI user:"
+#ovhai me
+
+# List the notebook for attendee
+ovhai notebook list --token $AI_TOKEN
