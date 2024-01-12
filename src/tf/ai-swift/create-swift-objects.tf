ba851a51f3da7b1d22aa5097e245729c55cb6ba4
Stéphane Philippart <wildagsx@gmail.com>
2024-01-12 17:31:55 +0100

    Tf s3 (#78)
    * clean: 🗑️ delete old scripts
    
    * doc: 🖼️ architecture overview
    
    * feat: 📀 S3 buckets creation with TF
    
    * fix: 🐛 use var

--- src/tf/ai-swift/create-swift-objects.tf
+++
@@ -1,4 +0,0 @@
-resource "openstack_objectstorage_container_v1" "ai-object-storage-container" {
-  count = var.number_of_attendees
-  name   = "attendee-${count.index}-data"
-}
--- src/tf/ai-users/create-users.tf
+++
@@ -1,5 +0,0 @@
-resource "ovh_cloud_project_user" "ai-user" {
-  count = var.number_of_users
-  role_names = ["ai_training_operator", "objectstore_operator"]
-  description = "AI user ${count.index} for workshop"
-}
