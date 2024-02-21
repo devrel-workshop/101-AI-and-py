4738fce9eb00272cd056f68fe722988a1e2366fc
Stéphane Philippart <wildagsx@gmail.com>
2024-02-21 17:50:30 +0100

    feat: 🗑️ Force S3 bucket deletion (#83)

--- src/tf/ai-s3/create-s3-buckets.tf
+++ src/tf/ai-s3/create-s3-buckets.tf
@@ -2,6 +2,7 @@
 #     Bucket
 ########################################################################################
 resource "aws_s3_bucket" "ai-s3" {
+  force_destroy = true
   count = var.number_of_attendees
   bucket = "${var.team}-${count.index}"
 }
