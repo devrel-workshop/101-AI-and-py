e815633658f9ae5dab56f81d2b3d8d34a3591e67
philippart-s <wildagsx@gmail.com>
2025-03-18 18:16:14 +0100

    feat: ✨ use pci project

--- setup_env.sh
+++ setup_env.sh
@@ -11,20 +11,20 @@ STUDENT_ID=$(jq -r '.teamName' "attendee-conf.json")
 REGISTRY_NAME=ubf2r2if.c1.gra9.container-registry.ovh.net/lab
 REGISTRY_LOGIN=$(jq -r '.registryLogin' "attendee-conf.json")
 REGISTRY_PASSWORD=$(jq -r '.registryPassword' "attendee-conf.json")
-AI_TOKEN=$(jq -r '.token' "attendee-conf.json")
+#AI_TOKEN=$(jq -r '.token' "attendee-conf.json")
 
 echo "Your environment variables:"
 echo "   - STUDENT_ID: " $STUDENT_ID
 echo "   - REGISTRY_NAME: " $REGISTRY_NAME
 echo "   - REGISTRY_LOGIN: " $REGISTRY_LOGIN
 echo "   - REGISTRY_PASSWORD: " $REGISTRY_PASSWORD
-echo "   - AI_TOKEN: " $AI_TOKEN
+#echo "   - AI_TOKEN: " $AI_TOKEN
 
-echo "Your notebook URL: " $(jq -r '.notebookUrl' "attendee-conf.json")
+#echo "Your notebook URL: " $(jq -r '.notebookUrl' "attendee-conf.json")
 
 # List the notebook for attendee
-echo ovhai notebook list --token $AI_TOKEN
-ovhai notebook list --token $AI_TOKEN
+echo ovhai notebook list 
+ovhai notebook list
 
 # create and activate a python venv
 python3 -m venv .venv
